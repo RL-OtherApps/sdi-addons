17e7a0fa319dcb95a6adfdbe483bd28711024cd0
JorgeQuinteros <quinterosorellana04@gmail.com>
2020-03-06 15:06:57 +0100

    [ADD] mail invoice reply to

---
+++ mail_invoice_reply_to/models/account_invoice.py
@@ -0,0 +1,15 @@
+###############################################################################
+# For copyright and license notices, see __manifest__.py file in root directory
+###############################################################################
+from odoo import api, models
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.multi
+    def _notify_get_reply_to(
+            self, default=None, records=None, company=None, doc_names=None):
+        import wdb;wdb.set_trace()
+        res = {account.id: self.env.user.id for account in self}
+        return res
